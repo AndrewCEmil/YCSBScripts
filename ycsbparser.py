8fb46fc7253ec41c4a96db321f6d044ed329338a
AndrewCEmil <andrew.emil@10gen.com>
2013-06-12 13:26:43 -0700

    Initial commit, script will take output file from ycsb and do a little bit of parsing and give you histogram of latencies

---
+++ ycsbparser.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+
+filename = "/Users/ace/YCSB/testout"
+lineheader = "[{0}], "
+RunTimeMs = -1
+throughput = -1
+infile = open(filename)
+
+#skip a few header lines
+infile.readline() #YCSB Client 0.1
+infile.readline() #Command line: ...
+infile.readline() #new database url = ...
+infile.readline() #mongo connection created
+
+#overall section
+sectionname = "OVERALL"
+runtime_line = infile.readline()
+if not runtime_line.startswith(lineheader.format(sectionname)):
+    print "error!!!!" #TODO turn to exception
+RunTimeMs = float(runtime_line.split(',')[2])
+runtime_line = infile.readline()
+if not runtime_line.startswith(lineheader.format(sectionname)):
+    print "error!!!!" #TODO turn to exception
+throughput = float(runtime_line.split(',')[2])
+print RunTimeMs
+print throughput 
+
+#loop over normal histogram data
+sectionnames = ["UPDATE", "READ"]
+sectionidx = 0
+currentline = infile.readline()
+results = { "UPDATE" : { "histogram" : [] }, "READ": { "histogram" : [] } }
+newsection = True
+while True:
+    cursection = sectionnames[sectionidx]
+    if newsection:
+        #handle the header for the new section
+        results[cursection]["ops"] = currentline.strip().split(',')[2]
+        results[cursection]["avglat"] = float(infile.readline().strip().split(',')[2])
+        results[cursection]["minlat"] = float(infile.readline().strip().split(',')[2])
+        results[cursection]["maxlat"] = float(infile.readline().strip().split(',')[2])
+        results[cursection]["95lat"] = float(infile.readline().strip().split(',')[2])
+        results[cursection]["99lat"] = float(infile.readline().strip().split(',')[2])
+        #TODO what is this next line?
+        infile.readline()
+        currentline = infile.readline()
+    while(currentline.startswith(lineheader.format(cursection))):
+        results[cursection]["histogram"].append(currentline.split(',')[2])
+        currentline = infile.readline()
+    sectionidx += 1
+    if sectionidx >= len(sectionnames):
+        break
+
+plt.plot(results["UPDATE"]["histogram"][0:5])
+#plt.plot(histograms["READ"])
+plt.show()
