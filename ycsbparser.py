4db80d073ad4f7ed1f24905813181b310fbc093d
AndrewCEmil <andrew.emil@10gen.com>
2013-06-17 17:33:39 -0700

    got parser working in an updated way

--- ycsbparser.py
+++ ycsbparser.py
@@ -1,60 +1,80 @@
 import pymongo
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 from datetime import datetime
 
-filename = "/Users/ace/perftesting/testouts/newVolTest4"
-lineheader = "[{0}], "
-RunTimeMs = -1
-throughput = -1
-infile = open(filename)
+pathname = "/home/ec2-user/"
+elenum = "0"
 
-#skip a few header lines
-infile.readline() #YCSB Client 0.1
-infile.readline() #Command line: ...
-infile.readline() #new database url = ...
-infile.readline() #mongo connection created
+def fullrun():
+    #generate filenames
+    loadfilename = pathname + "loadout" + elenum
+    testfilename = pathname + "testout" + elenum
+    #TODO try/catch
+    print "opening files..."
+    loadfile = open(loadfilename)
+    testfile = open(testfilename)
+    print "parsing load file"
+    loadres = parseout(loadfile, True)
+    print "parsing test file"
+    testres = parseout(testfile, False)
+    print "LOADRES:"
+    print loadres
+    print "TESTRES:"
+    print testres
 
-#overall section
-sectionname = "OVERALL"
-runtime_line = infile.readline()
-if not runtime_line.startswith(lineheader.format(sectionname)):
-    print "error!!!!" #TODO turn to exception
-RunTimeMs = float(runtime_line.split(',')[2])
-runtime_line = infile.readline()
-if not runtime_line.startswith(lineheader.format(sectionname)):
-    print "error!!!!" #TODO turn to exception
-throughput = float(runtime_line.split(',')[2])
-print RunTimeMs
-print throughput 
+def parseout(infile, isload):
+    #skip a few header lines
+    print "looping over headers"
+    currentline = infile.readline()
+    while("OVERALL" not in currentline):
+        currentline = infile.readline()
 
-#loop over normal histogram data
-sectionnames = ["UPDATE", "READ"]
-sectionidx = 0
-currentline = infile.readline()
-results = { "UPDATE" : { "histogram" : [] }, "READ": { "histogram" : [] } }
-#forgot to put this in before
-results["OVERALL"] = { "throughput" : throughput, "RunTimeMs" : RunTimeMs}
-newsection = True
-while True:
-    cursection = sectionnames[sectionidx]
-    if newsection:
-        #handle the header for the new section
+    #overall section
+    print "handling overall section"
+    sectionname = "OVERALL"
+    RunTimeMs = float(currentline.split(',')[2])
+    currentline = infile.readline()
+    throughput = float(currentline.split(',')[2])
+    results = { "OVERALL" : { "throughput" : throughput, "RunTimeMs" : RunTimeMs}}
+
+    #loop over normal histogram data
+    if isload:
+        sectionnames = ["INSERT"]
+        results["INSERT"] = {}
+    else:
+        sectionnames = ["UPDATE", "READ"]
+        results["UPDATE"] = {}
+        results["READ"] = {}
+
+    print "handling main data"
+    sectionidx = 0
+    currentline = infile.readline()
+    while True:
+        print "handling header data"
+        cursection = sectionnames[sectionidx]
+        #first handle the header for the new section
         results[cursection]["ops"] = currentline.strip().split(',')[2]
         results[cursection]["avglat"] = float(infile.readline().strip().split(',')[2])
         results[cursection]["minlat"] = float(infile.readline().strip().split(',')[2])
         results[cursection]["maxlat"] = float(infile.readline().strip().split(',')[2])
         results[cursection]["95lat"] = float(infile.readline().strip().split(',')[2])
         results[cursection]["99lat"] = float(infile.readline().strip().split(',')[2])
-        #TODO what is this next line?
+
+        #TODO what is this next line? for now skip it
         infile.readline()
+
+        print "handling histogram data"
         currentline = infile.readline()
-    while(currentline.startswith(lineheader.format(cursection))):
-        results[cursection]["histogram"].append(currentline.strip().split(',')[2])
-        currentline = infile.readline()
-    sectionidx += 1
-    if sectionidx >= len(sectionnames):
-        break
+        while(cursection in currentline):
+            #results[cursection]["histogram"].append(currentline.strip().split(',')[2])
+            #for now just skip the histogram lines
+            currentline = infile.readline()
+        sectionidx += 1
+        if sectionidx >= len(sectionnames):
+            break
+    print "returning results"
+    return results
 
 #plt.plot(results["UPDATE"]["histogram"][0:5])
 #plt.plot(histograms["READ"])
@@ -67,4 +87,4 @@ while True:
 #results['ts'] = datetime.now()
 #coll.insert(results)
 
-print results
+fullrun()
