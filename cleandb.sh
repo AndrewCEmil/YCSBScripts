3e490b313b3e9ef8fcfd6f41397a600f60442ecc
AndrewCEmil <andrew.emil@10gen.com>
2013-06-18 15:57:31 -0700

    fixed up the cleandb.sh file

--- cleandb.sh
+++ cleandb.sh
@@ -2,7 +2,9 @@
 
 DBPATH="/mnt/ebs/data/"
 LOGPATH="/mnt/ebs/data/log"
+JPATH="/mnt/dur/"
 MDPATH="/home/ec2-user/mongodb-linux-x86_64-2.4.4/bin/mongod"
+JLINK=$DBPATH"journal"
 
 pgrep mongo | xargs sudo kill
 
@@ -11,6 +13,8 @@ rm -rf $DBPATH
 mkdir $DBPATH
 rm $LOGPATH
 #TODO how to deal with the symlinked journal?
+rm -rf $JPATH*
+sudo ln -s $JPATH $JLINK
 
 #next we turn on mongod
 $MDPATH --dbpath $DBPATH --logpath $LOGPATH --fork 
